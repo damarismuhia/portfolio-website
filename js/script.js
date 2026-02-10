cb04ace5b8982106069770733b6ee1a38508fb3a
damarismuhia <muhiadamaris12@gmail.com>
2026-02-10 22:49:53 +0300

    skill automation

---
+++ js/script.js
@@ -0,0 +1,95 @@
+
+function populateSkillsSet() {
+     const skills =  [
+        {
+            iconName: ['fa-brands', 'fa-apple'],
+            skillName: '-Native iOS-',
+
+            skillsList : [
+                'Swift', 'UIKit' , 'SwiftUI', 'Combine', 'Async/Await', 'CoreData', 'Unit Testing'
+
+            ]
+
+        },
+        {
+            iconName: ['fa-brands', 'fa-android'],
+            skillName: '-Native Android-',
+
+            skillsList : [
+                'Kotlin', 'Java' , 'Jetpack Compose', 'XML Layout', 'Jetpack Libraries', 'Unit Testing'
+
+            ]
+
+        },
+        {
+            iconName: ['fa-solid', 'fa-laptop-code'],
+            skillName: '-Web Development-', 
+
+            skillsList : [
+                'JavaScript', 'CSS' , 'HTML5', 'React', 'Unit Testing'
+
+            ]
+
+        },
+        {
+            iconName: ['fa-solid', 'fa-screwdriver-wrench'],
+            skillName: '-Tools & Automation-',
+
+            skillsList : [
+                'Xcode', 'Android Studio' , 'VS Code', 'Git', 'CI/CD(Github Actions & Fastlane)', 'Unit Testing'
+
+            ]
+
+        }
+     ]
+
+     skills.forEach ( (item) => {
+        createDOMElementsForSkills(item)
+
+     })
+
+    
+    
+
+}
+
+function createDOMElementsForSkills(skill){
+    const skillGrid = document.querySelector('.skill-grid')
+    const skillDiv = document.createElement('div')
+    skillDiv.classList.add('skill-card')
+
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('skill-img-wrapper');
+    const icon = document.createElement('i');
+    icon.classList.add(...skill.iconName)
+    imgDiv.appendChild(icon)
+    skillDiv.appendChild(imgDiv);
+        
+
+    const crdTitleH3 = document.createElement('h3');
+    crdTitleH3.appendChild(document.createTextNode(skill.skillName));
+    skillDiv.appendChild(crdTitleH3);
+
+
+    const skilsUl = document.createElement('ul');
+    skilsUl.classList.add('skill-list');
+    skill.skillsList.forEach(item => {
+        const skillLi = document.createElement('li');
+        skillLi.appendChild(document.createTextNode(item));
+        skilsUl.appendChild(skillLi);
+    });
+    skillDiv.appendChild(skilsUl);
+    skillGrid.appendChild(skillDiv);
+    
+}
+
+
+
+
+
+
+function init(){
+    populateSkillsSet()
+}
+
+document.addEventListener('DOMContentLoaded', init)
